7301a06a7cd6233e09c0d2ab93f203d1dea658b4
Michael <michael.ramage@centre.edu>
2020-10-26 16:11:12 -0400

    just added a test for the component we did implement

--- src/main/java/Rello/cardTest.java
+++ src/main/java/Rello/cardTest.java
@@ -17,7 +17,6 @@ class cardTest
 	@BeforeEach
 	void setUp() throws Exception
 	{
-
 		// Set up users
 		users_instance = Users.getInstance(); // start users
 			
@@ -95,6 +94,7 @@ class cardTest
 		users_instance.resetInstance();
 	}
 	
+	@Test
 	void testRemoveCardMember() {
 		Card c1 = week1list.addCard("CSC Homework PG 10");
 		User olly = users_instance.addUser("olly@gmail.com","olly123");
@@ -102,6 +102,59 @@ class cardTest
 		c1.addMember(olly, jim); 
 		assertTrue(c1.getMembers().containsKey("olly@gmail.com"));		
 		c1.removeMember(olly, jim); 
-		assertTrue(!c1.getMembers().containsKey("olly@gmail.com"));		
+		assertTrue(!c1.getMembers().containsKey("olly@gmail.com"));
+		users_instance.resetInstance();
 	}
+	
+	@Test 
+	void testAddLabel() {
+		// add some cards to lists
+		Card cschwk1 = week1list.addCard("CSC Homework PG 10");
+		
+		// create and add labels to cards
+		Colors green = Colors.GREEN; 
+		Label green_label = new Label(green, "complete");
+		cschwk1.addLabel(green_label);
+		assertTrue(cschwk1.getLabels().containsKey(green));
+		assertTrue(cschwk1.getLabels().get(green).getText().equals("complete"));
+		users_instance.resetInstance();
+	}
+	
+	@Test 
+	void testRemoveLabel() {
+		// add some cards to lists
+		Card cschwk1 = week1list.addCard("CSC Homework PG 10");
+		
+		// create and add labels to cards
+		Colors green = Colors.GREEN; 
+		Label green_label = new Label(green, "complete");
+		
+		// add label and make sure it is here
+		cschwk1.addLabel(green_label);
+		assertTrue(cschwk1.getLabels().containsKey(green));
+		assertTrue(cschwk1.getLabels().get(green).getText().equals("complete"));
+		
+		// remove label
+		cschwk1.removeLabel(green);
+		// make sure it is no longer here 
+		assertTrue(!cschwk1.getLabels().containsKey(green));	
+		users_instance.resetInstance();
+	}
+	
+	// component testing for card
+	@Test
+	void testDescriptionComponent() {
+		Card cschwk1 = week1list.addCard("CSC Homework PG 10");
+		// no components on card yet --> size 0
+		assertTrue(cschwk1.getComponents().size() == 0);
+		
+		// add description component --> to size 1
+		Component desc1 = new Description("Read from page 10 to 19");
+		cschwk1.addComponent(desc1);
+		assertTrue(cschwk1.getComponents().size() == 1);
+		
+		// remove description component --> back to size 0
+		cschwk1.removeComponent(0);
+		assertTrue(cschwk1.getComponents().size() == 0);
+	}	
 }
